e8abede026914f354f970e6f815d29498eedca63
Leo <cheerleaderleo@outlook.com>
2026-08-05 00:31:26 +0800

    ci: isolate vmspawn repair mutation workflow

--- src/libsystemd-network/icmp6-test-util.h
+++
@@ -1,5 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-extern int test_fd[2];
-extern int test_ifindex;
--- src/systemctl/systemctl-list-unit-files.h
+++
@@ -1,6 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-#include "forward.h"
-
-int verb_list_unit_files(int argc, char *argv[], uintptr_t _data, void *userdata);
