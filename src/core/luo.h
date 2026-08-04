4bb39330df448db9732d94dd82fb17ff1dc7194d
Leo <cheerleaderleo@outlook.com>
2026-08-05 00:55:02 +0800

    ci: isolate final vmspawn direct repair

--- src/core/luo.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-#include "core-forward.h"
-
-int manager_luo_restore_fd_stores(Manager *m);
-int manager_luo_serialize_fd_stores(Manager *m, FILE **ret_f, FDSet **ret_fds);
