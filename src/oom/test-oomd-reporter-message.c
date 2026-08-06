750be409b2003e11d9f0275015a5f4b39827043a
Leo <cheerleaderleo@outlook.com>
2026-08-06 21:15:19 +0800

    test: cover compatible auto reporter withdrawals

--- src/oom/test-oomd-reporter-message.c
+++ src/oom/test-oomd-reporter-message.c
@@ -27,6 +27,7 @@ TEST(empty_report_is_an_explicit_empty_snapshot) {
         parse_parameters("{\"cgroups\":[]}", &parameters);
         ASSERT_OK(oomd_reporter_message_parse(parameters, &message));
         ASSERT_EQ(oomd_reporter_message_size(message), 0U);
+        assert_se(!oomd_reporter_message_entries(message));
 }
 
 TEST(valid_message_is_owned_and_normalized) {
@@ -65,6 +66,30 @@ TEST(valid_message_is_owned_and_normalized) {
         assert_se(streq(entries[2].value->rules[1], "batch"));
 }
 
+TEST(auto_with_configured_pressure_fields_is_a_withdrawal) {
+        _cleanup_(sd_json_variant_unrefp) sd_json_variant *parameters = NULL;
+        _cleanup_(oomd_reporter_message_freep) OomdReporterMessage *message = NULL;
+        const OomdPolicySnapshotEntry *entries;
+
+        parse_parameters(
+                        "{\"cgroups\":[{"
+                        "\"mode\":\"auto\","
+                        "\"path\":\"/x\","
+                        "\"property\":\"ManagedOOMMemoryPressure\","
+                        "\"limit\":5000,"
+                        "\"duration\":2000000"
+                        "}]}",
+                        &parameters);
+
+        ASSERT_OK(oomd_reporter_message_parse(parameters, &message));
+        ASSERT_EQ(oomd_reporter_message_size(message), 1U);
+        entries = oomd_reporter_message_entries(message);
+        assert_se(entries);
+        ASSERT_EQ(entries[0].property, OOMD_POLICY_MEMORY_PRESSURE);
+        assert_se(streq(entries[0].path, "/x"));
+        assert_se(!entries[0].value);
+}
+
 TEST(malformed_later_element_rejects_the_complete_message) {
         _cleanup_(sd_json_variant_unrefp) sd_json_variant *parameters = NULL;
         _cleanup_(oomd_reporter_message_freep) OomdReporterMessage *message = NULL;
@@ -91,8 +116,8 @@ TEST(non_object_element_is_rejected) {
 
 TEST(unknown_or_incompatible_fields_are_rejected) {
         _cleanup_(sd_json_variant_unrefp) sd_json_variant *unknown_property = NULL;
-        _cleanup_(sd_json_variant_unrefp) sd_json_variant *auto_payload = NULL;
         _cleanup_(sd_json_variant_unrefp) sd_json_variant *unknown_field = NULL;
+        _cleanup_(sd_json_variant_unrefp) sd_json_variant *swap_payload = NULL;
         _cleanup_(oomd_reporter_message_freep) OomdReporterMessage *message = NULL;
 
         parse_parameters(
@@ -102,15 +127,24 @@ TEST(unknown_or_incompatible_fields_are_rejected) {
         assert_se(!message);
 
         parse_parameters(
-                        "{\"cgroups\":[{\"mode\":\"auto\",\"path\":\"/x\",\"property\":\"ManagedOOMSwap\",\"limit\":1}]}",
-                        &auto_payload);
-        ASSERT_ERROR(oomd_reporter_message_parse(auto_payload, &message), EINVAL);
-        assert_se(!message);
-
-        parse_parameters(
                         "{\"cgroups\":[{\"mode\":\"kill\",\"path\":\"/x\",\"property\":\"ManagedOOMSwap\",\"extra\":true}]}",
                         &unknown_field);
         ASSERT_LT(oomd_reporter_message_parse(unknown_field, &message), 0);
+        assert_se(!message);
+
+        parse_parameters(
+                        "{\"cgroups\":[{\"mode\":\"kill\",\"path\":\"/x\",\"property\":\"ManagedOOMSwap\",\"limit\":1}]}",
+                        &swap_payload);
+        ASSERT_ERROR(oomd_reporter_message_parse(swap_payload, &message), EINVAL);
+        assert_se(!message);
+}
+
+TEST(parse_failure_clears_the_output_pointer) {
+        _cleanup_(sd_json_variant_unrefp) sd_json_variant *parameters = NULL;
+        _cleanup_(oomd_reporter_message_freep) OomdReporterMessage *message = (OomdReporterMessage*) UINT_TO_PTR(1);
+
+        parse_parameters("{\"cgroups\":[42]}", &parameters);
+        ASSERT_ERROR(oomd_reporter_message_parse(parameters, &message), EINVAL);
         assert_se(!message);
 }
 
@@ -120,8 +154,8 @@ TEST(duplicate_property_path_keys_are_rejected) {
 
         parse_parameters(
                         "{\"cgroups\":["
-                        "{\"mode\":\"kill\",\"path\":\"/x\",\"property\":\"ManagedOOMSwap\"},"
-                        "{\"mode\":\"auto\",\"path\":\"/x\",\"property\":\"ManagedOOMSwap\"}"
+                        "{\"mode\":\"kill\",\"path\":\"\",\"property\":\"ManagedOOMSwap\"},"
+                        "{\"mode\":\"auto\",\"path\":\"/\",\"property\":\"ManagedOOMSwap\"}"
                         "]}",
                         &parameters);
 
